df979c295b6afe8889dd82d8b06c19fc018a9a12
kyle fisher and zachary baum <kyle@>
2016-10-19 08:49:53 -0400

    create initial angular file structure

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="container">
+      <h1>Music Store</h1>
+    </div>
+  `
+})
+
+export class AppComponent {
+
+}
